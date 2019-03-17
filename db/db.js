ae8e369bcbc50ab4c8861dbd26733a594bd984ef
abduwibr <abduwibr@gmail.com>
2019-03-17 22:22:35 +0300

    homework done

---
+++ db/db.js
@@ -0,0 +1,14 @@
+const todos =  [
+    {
+      id: 1,
+      title: "lunch",
+      description: "Go for lunch by 2pm"
+      
+    },
+    {
+        "title": "breakfast",
+        "description": "get breakfast"
+    }
+];
+
+export default todos;
